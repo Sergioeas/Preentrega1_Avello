ad4b29575e80961f03be80cb3eec34c17a5f97d3
Sergio Avello <savello.eas96@gmail.com>
2024-06-10 23:42:36 -0400

    2da pre-entrega

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Inventario de bodega
 
+//___________________________DEFINO VARIABLES 
+
 let usuario;
-let disponibilidad = 100;
 let option;
 
 usuario = function (nombre, apellido) {
@@ -10,37 +11,183 @@ usuario = function (nombre, apellido) {
 }
 
 
+//_________________________ CREO UNA CLASE PARA HACER OBJETOS 
+
+class Producto {
+    constructor(codigo, nombre, marca, modelo, cantidad, descripcion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion
+    }
+    mostrarPropiedades() {
+        return `Código: ${this.codigo}\nNombre: ${this.nombre}\nMarca: ${this.marca}\nModelo: ${this.modelo}\nCantidad: ${this.cantidad}\nDescripción: ${this.descripcion}\n\n`;
+    }
+}
+
+
+let almohadas = new Producto ("0001", "Almohada","Rozen", "standard", 35, "Almohada de plumas" )
+
+let colchones = new Producto ("0002", "Colchon", "rozen", "BiPlz/Opdic", 35, "Colchon ortopedico de 2 plazas")
+
+let productosArray = [almohadas, colchones];
+
+
+//___________________________ AQUI CREO FUNCIONES PARA SUMAR Y RESTAR :)
+
+let resta = (a, b) => a - b
+
+let suma = (a, b) => a + b
+
+//___________ESTA FUNCION LA TUVE QUE CREAR PARA MOSTRAR LAS PRPIEDADES DE LOS OBJETOS DE LOS ARRAYS
+
+function mostrarProductos(productos) {
+
+    let resultado = "";
+
+    productos.forEach(producto => {
+        resultado += producto.mostrarPropiedades();
+    });
+
+    alert(resultado);
+}
+
+//_____________________________AQUI COMIENZA EL BUCLE
+
+
 let nombreUsuario = usuario(prompt("Ingrese su primer nombre"), prompt("Ingrese su primer apellido"))
 
 
 while (option != 0) {
-    option = parseInt(prompt("Usuario :" + nombreUsuario + " \n\n Menú de inventario \n\n Ingrese una opción \n 1.-Disponibilidad de almohadas \n 2.-Venta de almohadas \n 3.- Ingreso de almohadas \n 0.- Salir"
-    )) 
+    option = parseInt(prompt("Usuario :" + nombreUsuario + " \n\n Menú de inventario \n\n Ingrese una opción \n 1.-Disponibilidad de producto \n 2.-Salida de productos \n 3.- Ingreso de productos\n 4.- Ver detalle de productos \n 0.- Salir"
+    ))
+
+    if (isNaN(option)) {
+        alert("Por favor, ingrese una opción válida.");
+        continue; 
+    }
+
+//_____________________________ OPCION 1 PARA MOSTRAR CANTIDADES EN BODEGA
+
     if (option == 1) {
-        alert("Hay " + disponibilidad + " almohadas disponibles en bodega.-");
+
+        alert(`Hay ${almohadas.cantidad} almohadas disponibles en bodega.\nHay ${colchones.cantidad} colchones disponibles en bodega.`);
+
+
+//____________OPCION 2 PARA SACAR COSAS DE LA BODEGA... A SU VEZ TIENE 2 SUB-MENUS PARA ALMOHADAS Y COLCHONES
+
+
     } else if (option == 2) {
-        let retiro = parseInt(prompt("Ingrese la cantidad de almohadas vendidas o retiradas.-"));
-        if (isNaN(retiro) || retiro < 0) {
+
+        let menuRetiro = parseInt(prompt(`Qué producto que desea retirar... Digite la opción correspondiente: \n
+            1.- Almohadas
+            2.- Colchones`));
+
+        if (isNaN(menuRetiro) || menuRetiro < 0 || menuRetiro > 2) {
             alert("Por favor ingrese una cantidad válida.-");
-        } else if (retiro > disponibilidad) {
-            alert("No hay sificientes almohadas en existencia.-");
-        } else {
-            disponibilidad = disponibilidad - retiro;
-            alert("Ha retirado " + retiro + " almohadas de la bodega.-");
-            alert("Quedan " + disponibilidad + " almohadas en bodega.-");
+
+        } else if (menuRetiro == 1) {
+            let retiroAlmohadas = parseInt(prompt(`¿Cuantas Almohadas desea retirar de bodega?`));
+
+            if (retiroAlmohadas > almohadas.cantidad) {
+                alert("No hay suficientes almohadas en existencia.-");
+                
+            } else if (isNaN(retiroAlmohadas)) {
+                alert(`Por favor ingrese una cantidad valida`)
+            }
+            
+            else {
+
+                almohadas.cantidad = resta (almohadas.cantidad, retiroAlmohadas);
+                alert("Ha retirado " + retiroAlmohadas + " almohadas de la bodega.-");
+                alert("Quedan " + almohadas.cantidad + " almohadas en bodega.-");
+
+            }
+
+        } else if (menuRetiro == 2){
+
+            let retiroColchones = parseInt(prompt(`¿Cuantos colchones desea retirar?`))
+
+            if (retiroColchones > colchones.cantidad) {
+
+                alert(`No hay suficientes colchones en existencia`);
+
+            } else if (isNaN(retiroColchones)) {
+
+                alert(`Por favor ingrese una cantidad valida`)
+
+            } else {
+
+                colchones.cantidad = resta (colchones.cantidad, retiroColchones);
+                alert("Ha retirado " + retiroColchones + " colchones de la bodega.-");
+                alert("Quedan " + colchones.cantidad + " colchones en bodega.-");
+
+            }
         }
-    } else if (option == 3) {
-        let ingreso = parseInt(prompt("Indique la cantidad de almohadas que ingresan a bodega.-"));
-        if (isNaN(ingreso)) {
-            alert("Ingrese una cantidad válida.-")
-        } else {
-            disponibilidad = disponibilidad + ingreso;
-            alert("Ha ingresado " + ingreso + " almohadas a la bodega.-");
-            alert("Hay " + disponibilidad + " almohadas en bodega.-");
+    }   
+
+
+//__________EL MENU 3 ES PARA INGRESAR COSAS A LA BODEGA... TAMBIEN LE PUSE 2 SUB-MENUS PARA DIFERENCIAR PRODUCTOS
+
+
+        else if (option == 3) {
+        let menuIngreso = parseInt(prompt(`Qué producto que desea ingresar... Digite la opción correspondiente: \n
+            1.- Almohadas
+            2.- Colchones`));
+
+        if (isNaN(menuIngreso)|| menuIngreso < 1 || menuIngreso > 2) {
+            alert("Ingrese una opció válida.-")
+
+        } else if (menuIngreso == 1){
+
+            let ingresoAlmohadas = parseInt(prompt(`Indique la cantidad de almohadas que ingresan a bodega`));
+
+            if (isNaN(ingresoAlmohadas) || ingresoAlmohadas < 0) {
+
+                alert(`Ingrese una cantidad válida`)
+
+            } else {
+                almohadas.cantidad = suma (almohadas.cantidad, ingresoAlmohadas)
+                alert(`Ha ingresado ${ingresoAlmohadas} a bodega`)
+                alert(`Ahora existen ${almohadas.cantidad} en bodega`)
+            } 
+        } else if (menuIngreso == 2) {
+
+            let ingresoColchones = parseInt(prompt(`Indique la cantidad de colchones que ingresan a bodega`));
+
+            if (isNaN(ingresoColchones) || ingresoColchones < 0) {
+
+                alert(`Ingrese una cantidad válida`)
+
+            } else {
+                colchones.cantidad = suma (colchones.cantidad, ingresoColchones)
+                alert(`Ha ingresado ${ingresoColchones} a bodega`)
+                alert(`Ahora existen ${colchones.cantidad} en bodega`)
+            } 
         }
+
+//___________________LA OPCION 4 ES PARA MOSTRAR EL DETALLE DE LOS PRODUCTOS QUE EXISTEN
+
+
+    } else if (option == 4) {
+
+         mostrarProductos(productosArray);
+
+
+//__________________ YA AQUI DEFINO EL METODO DE CIERRE DE LA APLICACION 
+
     } else if (option == 0) {
+
         alert("Saliendo")
+
     } else {
+
         alert("Ingrese una opcion válida.-")
+
     }
 }
+
+
+
